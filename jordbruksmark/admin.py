0cbb50ed757b3c510e8916115bd35d08241ba94e
motu55 <david.simmen@bluewin.ch>
2016-12-17 22:38:00 +0100

    admin sites

---
+++ jordbruksmark/admin.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin, messages
+from jordbruksmark.models import *
+
+class ParzellenAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "name"]
+    search_fields = ["id", "code", "name"]
+    
+class DuengungsstufenAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__"]
+    search_fields = ["id", "name"]
+    
+class DuengerAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__"]
+    search_fields = ["id", "name"]
+    
+class FamilienAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__"]
+    search_fields = ["id", "name"]
+    
+class KulturenAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "familie_name"]
+    search_fields = ["id", "name", "familie__name"]
+    def familie_name(self, obj):
+        return obj.famili.name
+    familie_name.admin_order_field = 'familie__name'
+    
+class WochenMengenAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "menge"]
+    search_fields = ["id", "woche", "kultur__name"]
+    
+class SaetzeAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "sorte"]
+    search_fields = ["id", "sorte", "kultur__name", "nummer"]
+
+admin.site.register(Parzelle, ParzellenAdmin)
+admin.site.register(Duengungsstufe, DuengungsstufenAdmin)
+admin.site.register(Duenger, DuengerAdmin)
+admin.site.register(Familie, FamilienAdmin)
+admin.site.register(Kultur, KulturenAdmin)
+admin.site.register(WochenMenge, WochenMengenAdmin)
+admin.site.register(Satz,SaetzeAdmin)
--- jordbruksmark/models.py
+++ jordbruksmark/models.py
@@ -1,31 +1,45 @@
+# encoding: utf-8
+
 from django.db import models
 from django.core import validators
 
 
 class Parzelle(models.Model):
     code = models.CharField(u'Code', max_length=100, validators=[validators.validate_slug], unique=True)
-    name = models.CharField(u'Name', max_length=100, unique=True)
+    name = models.CharField(u'Name', max_length=100, unique=True)    
+    
+    def __unicode__(self):
+        return u"(%s)%s" % (self.id, self.code)
     
     class Meta:
         verbose_name = u'Parzelle'
         verbose_name_plural = u'Parzellen'
         
 class Duengungsstufe(models.Model):
-    name = models.CharField(u'Name', max_length=100, unique=True)
+    name = models.CharField(u'Name', max_length=100, unique=True)  
+    
+    def __unicode__(self):
+        return u"(%s)%s" % (self.id, self.name)
     
     class Meta:
-        verbose_name = u'Duengungsstufe'
-        verbose_name_plural = u'Duengungsstufe'
+        verbose_name = u'Düngungsstufe'
+        verbose_name_plural = u'Düngungsstufe'
         
 class Duenger(models.Model):
     name = models.CharField(u'Name', max_length=100, unique=True)
     
+    def __unicode__(self):
+        return u"(%s)%s" % (self.id, self.name)
+    
     class Meta:
-        verbose_name = u'Duenger'
-        verbose_name_plural = u'Duenger'
+        verbose_name = u'Dünger'
+        verbose_name_plural = u'Dünger'
         
 class Familie(models.Model):
-    name = models.CharField(u'Name', max_length=100, unique=True)
+    name = models.CharField(u'Name', max_length=100, unique=True)    
+    
+    def __unicode__(self):
+        return u"(%s)%s" % (self.id, self.name)
     
     class Meta:
         verbose_name = u'Familie'
@@ -34,11 +48,27 @@ class Familie(models.Model):
 class Kultur(models.Model):
     name = models.CharField(u'Name', max_length=100, unique=True)
     familie = models.ForeignKey(Familie, related_name='kulturen', null=False, blank=False,
-                                 on_delete=models.PROTECT)
+                                 on_delete=models.PROTECT)   
+                                 
+    def __unicode__(self):
+        return u"(%s)%s" % (self.id, self.name)
     
     class Meta:
         verbose_name = u'Kultur'
         verbose_name_plural = u'Kulturen'
+        
+class WochenMenge(models.Model):
+    woche = models.IntegerField(u'Wochennummer')
+    menge = models.IntegerField(u'Menge')
+    kultur = models.ForeignKey(Kultur, related_name='kulturen', null=False, blank=False,
+                                 on_delete=models.PROTECT)    
+                                 
+    def __unicode__(self):
+        return u"(%s)%s - %s" % (self.id, self.kultur.name, self.woche)
+    
+    class Meta:
+        verbose_name = u'Wochen Menge'
+        verbose_name_plural = u'Wochen Mengen'
 
 class Satz(models.Model):
     nummer = models.IntegerField(u'Nummer')
@@ -59,7 +89,7 @@ class Satz(models.Model):
     saat = models.IntegerField(u'Saat',null=True,blank=True)
     erste_ernte = models.IntegerField(u'1. Ernte',null=True,blank=True)
     ernteschluss = models.IntegerField(u'Ernteschluss',null=True,blank=True)
-    duenger_kg = models.CharField(u'Duenger kg 10N', max_length=100,null=True,blank=True)
+    duenger_kg = models.CharField(u'Dünger kg %10N', max_length=100,null=True,blank=True)
     beet_breite = models.FloatField(u'g/1000 Pflanzen',null=True,blank=True)
     reihen_abstand = models.IntegerField(u'Reihenabstand',null=True,blank=True)
     pflanz_abstand = models.IntegerField(u'Pflanzabstand',null=True,blank=True)
@@ -68,10 +98,15 @@ class Satz(models.Model):
     bem_anzucht = models.TextField(u'Bem.  Anzucht', max_length=1000, null=True,blank=True)
     bem_setzen = models.TextField(u'Bem. Setzen', max_length=1000, null=True,blank=True)
     bem_saat = models.TextField(u'Bem. Direktsaat', max_length=1000, null=True,blank=True)
+    start_woche = models.IntegerField(u'Startwoche Menge',null=True,blank=True)
+    end_woche = models.IntegerField(u'Endwoche Menge',null=True,blank=True)
+    
+    def __unicode__(self):
+        return u"(%s)%s - %s" % (self.id, self.nummer, self.kultur.name)
     
     class Meta:
         verbose_name = u'Satz'
-        verbose_name_plural = u'Saetze'
+        verbose_name_plural = u'Sätze'
     
     
     
