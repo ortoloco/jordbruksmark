f7bf6fec0cf79893cad2b598aada412334cabba1
motu55 <david.simmen@bluewin.ch>
2017-01-12 16:56:02 +0100

    fix hor heroku

--- jordbruksmark/wsgi.py
+++ jordbruksmark/wsgi.py
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jordbruksmark.settings")
 
 application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
+
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
